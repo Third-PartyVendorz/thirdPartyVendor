85aaff0c04245d202fcd05126ddf6b1402f15675
Luke Sheldon <lukesheldon2004@gmail.com>
2026-09-15 13:12:33 +0000

    Connect to DB from python

---
+++ analytics/automation_testing.py
@@ -0,0 +1,28 @@
+import psycopg2
+
+print(psycopg2.__version__)
+
+try:
+    conn = psycopg2.connect(
+        dbname="tpvdb",
+        user="postgres",
+        password="postgres",
+        host="localhost",
+        port="5432"
+    )
+    print("Connection successful")
+    
+    query = "SELECT * FROM Users;"
+    
+    cur = conn.cursor()
+    cur.execute(query)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    conn.close()
+    
+except Exception as e:
+    print(f"Error: {e}")
+
+
