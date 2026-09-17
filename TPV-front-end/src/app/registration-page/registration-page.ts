262130c937e041311d34b49df66f6ce7d48c8119
Aimee Hong <honglp@bc.edu>
2026-09-17 20:12:36 +0000

    create registration component

---
+++ TPV-front-end/src/app/registration-page/registration-page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-registration-page',
+  styleUrl: './registration-page.scss',
+  templateUrl: './registration-page.html',
+})
+export class RegistrationPage {
+}
