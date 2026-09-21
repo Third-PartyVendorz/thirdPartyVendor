46cb173d363d4ffc8d6a0cd9be57861a0446af85
AdamOuld <ouldsfiyaadam@gmail.com>
2026-09-21 19:18:23 +0000

    Implement auth service on frontend

---
+++ TPV-front-end/src/app/dto/RegisterRequest.ts
@@ -0,0 +1,8 @@
+export interface RegisterRequest {
+    firstName: string;
+    lastName: string;
+    phoneNumber: string;
+    dateOfBirth: Date;
+    email: string;
+    password: string;
+}
---
+++ TPV-front-end/src/app/services/auth.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { RegisterRequest } from '../dto/RegisterRequest';
+import { RegisterResponse } from '../dto/RegisterResponse';
+import { AuthenticationRequest } from '../dto/AuthenticationRequest';
+import { AuthenticationResponse } from '../dto/AuthenticationResponse';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  constructor(private http: HttpClient) { }
+
+  register(registerRequest: RegisterRequest) {
+    return this.http.post<RegisterResponse>('/auth/register', registerRequest);
+  }
+
+  authenticate(authenticationRequest: AuthenticationRequest) {
+    return this.http.post<AuthenticationResponse>('/auth/authenticate', authenticationRequest);
+  }
+
+}
