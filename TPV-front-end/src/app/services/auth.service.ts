1b26cec90faa50e2e87cba2b10cfffd35b8e629a
AdamOuld <ouldsfiyaadam@gmail.com>
2026-09-22 17:06:24 +0000

    Add environment file for ui

--- TPV-front-end/src/app/services/auth.service.ts
+++ TPV-front-end/src/app/services/auth.service.ts
@@ -4,6 +4,7 @@ import { RegisterRequest } from '../dto/RegisterRequest';
 import { RegisterResponse } from '../dto/RegisterResponse';
 import { AuthenticationRequest } from '../dto/AuthenticationRequest';
 import { AuthenticationResponse } from '../dto/AuthenticationResponse';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -12,11 +13,11 @@ export class AuthService {
   constructor(private http: HttpClient) { }
 
   register(registerRequest: RegisterRequest) {
-    return this.http.post<RegisterResponse>('/auth/register', registerRequest);
+    return this.http.post<RegisterResponse>(`${environment.apiBaseUrl}/auth/register`, registerRequest);
   }
 
   authenticate(authenticationRequest: AuthenticationRequest) {
-    return this.http.post<AuthenticationResponse>('/auth/authenticate', authenticationRequest);
+    return this.http.post<AuthenticationResponse>(`${environment.apiBaseUrl}/auth/authenticate`, authenticationRequest);
   }
 
   getAuthToken() {
