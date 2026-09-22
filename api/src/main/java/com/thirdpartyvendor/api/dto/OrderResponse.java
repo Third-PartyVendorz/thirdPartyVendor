3f5c2168a54f58ea89ab20cbdee71ea1b4dd41a0
Jack Marcello <jack1124marcello@gmail.com>
2026-09-22 15:15:35 -0400

    refactored the DTO to use records instead

--- api/src/main/java/com/thirdpartyvendor/api/dto/OrderResponse.java
+++ api/src/main/java/com/thirdpartyvendor/api/dto/OrderResponse.java
@@ -6,33 +6,26 @@ import com.thirdpartyvendor.api.entity.Order;
 import com.thirdpartyvendor.api.entity.Order.OrderIntent;
 import com.thirdpartyvendor.api.entity.Order.OrderStatus;
 
-public class OrderResponse {
-    private Long orderId;
-    private Long userId;
-    private Long assetId;
-    private OrderIntent orderIntent;
-    private BigDecimal quantity;
-    private BigDecimal orderPrice;
-    private OrderStatus status;
-    private LocalDateTime createdAt;
-
+public record OrderResponse(
+    Long orderId,
+    Long userId,
+    Long assetId,
+    OrderIntent orderIntent,
+    BigDecimal quantity,
+    BigDecimal orderPrice,
+    OrderStatus status,
+    LocalDateTime createdAt
+) {
     public OrderResponse(Order order) {
-        this.orderId = order.getId();
-        this.userId = order.getUserId();
-        this.assetId = order.getAssetId();
-        this.orderIntent = order.getOrderIntent();
-        this.quantity = order.getQuantity();
-        this.orderPrice = order.getOrderPrice();
-        this.status = order.getStatus();
-        this.createdAt = order.getCreatedAt();
+        this(
+            order.getId(),
+            order.getUserId(),
+            order.getAssetId(),
+            order.getOrderIntent(),
+            order.getQuantity(),
+            order.getOrderPrice(),
+            order.getStatus(),
+            order.getCreatedAt()
+        );
     }
-
-    public Long getOrderId() { return orderId; }
-    public Long getUserId() { return userId;}
-    public Long getAssetId() { return assetId; }
-    public OrderIntent getOrderIntent() { return orderIntent; }
-    public BigDecimal getQuantity() { return quantity; }
-    public BigDecimal getOrderPrice() { return orderPrice; }
-    public OrderStatus getStatus() { return status; }
-    public LocalDateTime getCreatedAt() { return createdAt; }
 }
