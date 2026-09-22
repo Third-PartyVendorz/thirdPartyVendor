ce388e6f5b05273a4450fa260edd991463188113
Robert Winfield <winfieldrl@gmail.com>
2026-09-22 20:44:02 +0000

    add v13 and v14

--- api/src/main/resources/db/migration/V13__add_order_currency_to_orders.sql
+++ api/src/main/resources/db/migration/V13__add_order_currency_to_orders.sql
@@ -1,4 +1,2 @@
--- Add order_currency to orders table to specify the currency of the order price
-
 ALTER TABLE orders
 ADD COLUMN order_currency VARCHAR(3);
--- api/src/main/resources/db/migration/V14__add_trade_currency_to_trade.sql
+++ api/src/main/resources/db/migration/V14__add_trade_currency_to_trade.sql
@@ -1,4 +1,2 @@
--- Add trade_currency to trade table to specify the currency in which the trade was executed
-
 ALTER TABLE trade
 ADD COLUMN trade_currency VARCHAR(3);
